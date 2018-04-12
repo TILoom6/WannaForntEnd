1e0405bd15c71a034ed0f812c520cc6f55449014
iida <perusemone@gmail.com>
2018-04-13 06:53:07 +0900

    ファイル名称変更　不要ファイルの消去

--- src/components/organizations/Wanna.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Wanna extends React.Component {
-  render() {
-    return <div>Wanna</div>;
-  }
-}
-
-export default Wanna;
---
+++ src/components/organizations/WannaList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class WannaList extends React.Component {
+  render() {
+    return <div>WannaList</div>;
+  }
+}
+
+export default Wanna;
